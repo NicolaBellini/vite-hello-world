cd2aa1402c9612078260056477816f82c782782e
NicolaBellini <bellininicola4366@gmail.com>
2024-03-20 14:09:20 +0100

    scuffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      title:'Hello word!'
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h1></h1>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
