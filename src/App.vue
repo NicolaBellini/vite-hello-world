8ba65a4c636218be97b7a1ab6f98b6824c38c69d
NicolaBellini <bellininicola4366@gmail.com>
2024-03-20 14:27:12 +0100

    [add] global styles for p

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,13 @@ export default {
 
 <template>
 
-<div
+  <div
     :class="{'hovered':isHovered}"
     class="container"
     @mouseenter="isHovered=true"
     @mouseleave="isHovered=false">
     <h1 class="mt-2">{{ title }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quibusdam consequatur expedita molestiae quidem, fugit minus maxime necessitatibus deserunt quaerat sunt? Doloremque, quisquam. Quisquam cupiditate nulla vero, nihil dignissimos ex exercitationem ducimus odio.</p>
   </div>
 
 </template>
@@ -30,5 +31,10 @@ export default {
 }
 .hovered{
 background-color: blue;
+}
+</style>
+<style>
+p{
+  color: sandybrown;
 }
 </style>
